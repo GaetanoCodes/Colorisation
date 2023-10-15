607898ea38d7d2804fa95c97f532211308646459
GaetanoCodes <gaetano.agazzotti@hotmail.fr>
2023-10-15 12:32:14 +0200

    .utils instead of utils

--- eccv16.py
+++ eccv16.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch.nn.functional as F
 from IPython import embed
 import matplotlib.pyplot as plt
-from utils import *
+from .utils import *
 import torchvision.transforms as T
 
 
