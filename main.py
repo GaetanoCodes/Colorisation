4f12d1bd67c1d497935e788cbbdb6a348552d9a4
GaetanoCodes <gaetano.agazzotti@hotmail.fr>
2023-10-20 11:09:30 +0200

    Poisson

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Video.utils import Video, build_video, DVP
 
 if __name__ == "__main__":
-    path = "Video/videos/poisson_court2.mp4"
+    path = "Video/videos/Poisson.mp4"
     video = DVP(path, size=(128, 128))
     video.optimize(0.1, 1)
     video.plot_an_image()
